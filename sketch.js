90b89bec897330e4995b138076decb572053330e
jhp922 <stan090981@gmail.com>
2025-05-29 08:58:44 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,43 +1,154 @@
-let standImg, walkImg;
-let characterX = 40;
-let characterY = 120;
+let handPose;
+let video;
+let hands = [];
+
+let objectX = 300, objectY = 400;
+let objectVisible = true;
+let isGrabbing = false;
+let offsetX = 0, offsetY = 0;
+
+let openHandImg, closedHandImg;
+let standImgs = [], walkImgs = [];
+
+let currentAge = 0;
+let characterX = 40, characterY = 400;
 let isGiven = false;
 let frameToggle = false;
 
 function preload() {
-  standImg = loadImage('child stand.png'); // 스탠딩 이미지 파일명
-  walkImg = loadImage('child walk.png');   // 걷기 이미지 파일명
+  handPose = ml5.handPose();
+  openHandImg = loadImage('openHand.png');
+  closedHandImg = loadImage('closedHand.png');
+
+  standImgs[0] = loadImage('child stand.png');
+  walkImgs[0]  = loadImage('child walk.png');
+  standImgs[1] = loadImage('teen walk.png');
+  walkImgs[1]  = loadImage('teen walk 2.png');
+  standImgs[2] = loadImage('adult walk.png');
+  walkImgs[2] = loadImage('adult walk2.png');
+  standImgs[3] = loadImage('old walk.png');
+  walkImgs[3] = loadImage('old walk2.png');
 }
 
 function setup() {
-  createCanvas(480, 240);
+  createCanvas(1600, 900);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  handPose.detectStart(video, gotHands);
   frameRate(20);
 }
 
 function draw() {
   background(220);
 
-  drawCharacter(characterX, characterY, isGiven);
+  drawCharacter(characterX, characterY, isGiven, currentAge);
+  drawHands();
 
-  // 예시: 물건 받으면 오른쪽으로 이동
   if (isGiven) {
-    characterX += 2;
+    characterX += 4;
+    if (characterX > width + 50) {
+      nextCharacter();
+    }
   }
-}
 
-// "캐릭터 그리기" 함수 정의
-function drawCharacter(x, y, isGiven) {
-  if (!isGiven) {
-    image(standImg, x, y);
-  } else {
-    if (frameCount % 6 == 0) frameToggle = !frameToggle; // 6프레임마다 교체
-    image(frameToggle ? walkImg : standImg, x, y);
+  if (objectVisible) {
+    drawRecyclingBox(objectX, objectY);
   }
-}
 
-// 마우스 클릭 시 물건을 받은 것으로 처리
-function mousePressed() {
-  if (!isGiven) {
+  updateHandState();
+  updateObjectPosition();
+
+  if (objectVisible && isNearCharacter(objectX, objectY)) {
+    objectVisible = false;
     isGiven = true;
   }
+}
+
+function drawCharacter(x, y, isGiven, ageIndex) {
+  if (!isGiven) {
+    image(standImgs[ageIndex], x, y, 128, 128);
+  } else {
+    if (frameCount % 6 === 0) frameToggle = !frameToggle;
+    image(frameToggle ? walkImgs[ageIndex] : standImgs[ageIndex], x, y, 128, 128);
+  }
+}
+
+function drawRecyclingBox(x, y) {
+  push();
+  noStroke();
+  const dotSize = 6;
+  fill(34, 139, 34);
+  for (let i = -3; i <= 3; i++) {
+    for (let j = -2; j <= 2; j++) {
+      rect(x + i * dotSize, y + j * dotSize, dotSize, dotSize);
+    }
+  }
+  pop();
+}
+
+function drawHands() {
+  for (let hand of hands) {
+    let indexTip = hand.keypoints.find(k => k.name === "index_finger_tip");
+    if (indexTip) {
+      imageMode(CENTER);
+      image(isGrabbing ? closedHandImg : openHandImg, indexTip.x, indexTip.y, 60, 60);
+    }
+  }
+}
+
+function updateHandState() {
+  for (let hand of hands) {
+    let thumbTip = hand.keypoints.find(k => k.name === "thumb_tip");
+    let indexTip = hand.keypoints.find(k => k.name === "index_finger_tip");
+
+    if (thumbTip && indexTip) {
+      let d = dist(thumbTip.x, thumbTip.y, indexTip.x, indexTip.y);
+      if (d < 40) {
+        if (!isGrabbing && objectVisible && isNearObject(indexTip.x, indexTip.y)) {
+          isGrabbing = true;
+          offsetX = objectX - indexTip.x;
+          offsetY = objectY - indexTip.y;
+        }
+      } else {
+        isGrabbing = false;
+      }
+    }
+  }
+}
+
+function updateObjectPosition() {
+  if (!isGrabbing || !objectVisible) return;
+
+  let hand = hands[0];
+  if (!hand) return;
+
+  let indexTip = hand.keypoints.find(k => k.name === "index_finger_tip");
+  if (indexTip) {
+    objectX = indexTip.x + offsetX;
+    objectY = indexTip.y + offsetY;
+  }
+}
+
+function nextCharacter() {
+  currentAge++;
+  if (currentAge >= standImgs.length) currentAge = 0;
+
+  characterX = 40;
+  isGiven = false;
+  objectVisible = true;
+  objectX = 300;
+  objectY = 400;
+}
+
+function isNearObject(x, y) {
+  return dist(x, y, objectX, objectY) < 50;
+}
+
+function isNearCharacter(x, y) {
+  return dist(x, y, characterX, characterY) < 50;
+}
+
+function gotHands(results) {
+  hands = results;
 }
